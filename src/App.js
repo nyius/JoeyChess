594a4db0a4eee037d7780bd2da8192a7ca95416e
Joseph Scicluna <josephdesigned@gmail.com>
2021-03-15 14:18:23 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './scss/index.scss';
+import { BOARD_HEIGHT, BOARD_WIDTH } from './config';
+
+class JoeyChess extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//
+		};
+	}
+
+	chessBoard() {
+		let board = [];
+		for (let i = 0; i < BOARD_WIDTH; i++) {
+			board.push(
+				<tr key={i + 1} className="board_square board_square--white"></tr>
+			);
+		}
+		console.log(board);
+		return board;
+	}
+
+	render() {
+		return (
+			<div className="board_container">
+				<this.chessBoard />
+			</div>
+		);
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <JoeyChess />;
 }
 
 export default App;
